6f26f6409649dab315cadb1e95dbfef8c8279f35
ofer221 <ofer2212@gmail.com>
2022-04-05 18:19:13 +0300

    slack messages

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import json
+import requests
 import os
 import re
 import boto3
@@ -6,6 +8,8 @@ from botocore.exceptions import NoCredentialsError
 
 ACCESS_KEY = 'XXXXXXXXXXXXXXXXXXXXXXX'
 SECRET_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+slack_general = 'XXXXXXXXXXXXXXXXXXXXXXX'
+slack_logs = 'XXXXXXXXXXXXXXXXXXXXXXX'
 
 
 def has_numbers(inputString):
@@ -13,19 +17,33 @@ def has_numbers(inputString):
     return bool(re.search(r'\d', res))
 
 
+def message_to_slack(msg, dest_url):
+    slack_message = {'text': msg}
+    response = requests.post(
+        dest_url, data=json.dumps(slack_message),
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+        logger('Slack', 'Failed - {0}'.format(response.text))
+    else:
+        logger('Slack', 'Success - Message sent')
+
+
+def logger(src, msg):
+    log_msg = 'LOG DATE:{0} \nLOG SOURCE:{1} \n LOG CONTENT:{2} \n'.format(datetime.now(), src, msg)
+    print(log_msg)
+    message_to_slack(log_msg, slack_logs)
+
+
 def upload_to_aws(local_file, bucket, s3_file):
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
                       aws_secret_access_key=SECRET_KEY)
-
     try:
         s3.upload_file(local_file, bucket, s3_file)
-        print("Upload Successful")
         return True
     except FileNotFoundError:
-        print("The file was not found")
         return False
     except NoCredentialsError:
-        print("Credentials not available")
         return False
 
 
@@ -37,14 +55,19 @@ def check_files():
         if os.path.isfile(f):
             # checking if it contains digits
             if has_numbers(f):
-                uploaded = upload_to_aws(f, 'invalid-bucket', '{0}-{1}'.format(f, datetime.now()))
-                print('The file {0} was invalid\n{1}'.format(filename, uploaded))
+                uploaded = upload_to_aws(f, 'invalid-bucket', '{0}-{1}'.format(filename, datetime.now()))
+                logger('File Checker', 'Failed - The file {0} was invalid'.format(filename))
+                message_to_slack('The file {0} is corrupted, uploaded to the invalid bucket', slack_general)
             else:
-                uploaded = upload_to_aws(f, 'valid-bucket', '{0}-{1}'.format(f, datetime.now()))
-                print('The file {0} was valid\n{1}'.format(filename, uploaded))
+                uploaded = upload_to_aws(f, 'valid-bucket', '{0}-{1}'.format(filename, datetime.now()))
+                logger('File Checker', 'Failed - The file {0} was valid'.format(filename))
+            if uploaded:
+                os.remove(f)
+                logger('AWS-S3', 'Upload Success')
+            else:
+                logger('AWS-S3', 'Failed to upload the file {0} to S3'.format(filename))
 
 
 if __name__ == '__main__':
-    print("CRON executed at - \n")
-    print(datetime.now().strftime("%D %H:%M:%S"))
+    print("CRON executed at - {0}\n".format(datetime.now().strftime("%D %H:%M:%S")))
     check_files()
