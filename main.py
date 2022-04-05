558501a6608d6f698f2f737ae0cd738cf5d07a9b
ofer221 <ofer2212@gmail.com>
2022-04-05 15:50:41 +0300

    testing file scanning

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from datetime import datetime
+import os
+
+
+def check_files():
+    directory = '/home/ubuntu/projects/uploads'
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            print(f)
+
 
 if __name__ == '__main__':
     print("CRON executed at - \n")
     print(datetime.now().strftime("%D %H:%M:%S"))
-
+    check_files()
